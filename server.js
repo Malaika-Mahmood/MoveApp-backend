ad6877c8850a847cff241251b6f82a3654c24e57
Malaika Mahmood <malaikamahmoodpro0@gmail.com>
2026-08-28 08:35:32 +0500

    fix host binding and dynamic port

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-
 const pool = require("./config/db");
-
+const authRoutes = require("./routes/authRoutes");
+const operatorRoutes = require("./routes/operatorRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1/auth", authRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/v1/operator", operatorRoutes);
 
 app.get("/", (req, res) => {
     res.json({
@@ -34,6 +37,7 @@ app.get("/test-db", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
